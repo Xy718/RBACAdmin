32fd69ae358f6be8e5b11487623c13b7daa1cba0
Administrator <Administrator@A027>
2020-07-27 22:05:22 +0800

    init

---
+++ src/main/java/xyz/xy718/RBACAdmin/RbacAdminApplication.java
@@ -0,0 +1,13 @@
+package xyz.xy718.RBACAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbacAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/xy718/RBACAdmin/RbacAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.xy718.RBACAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
